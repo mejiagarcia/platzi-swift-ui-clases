ab6cf8c2b84f681d53e6302798ad299f8b0f2e05
Carlos Mejia <carlosmejia083@gmail.com>
2020-01-23 12:32:18 -0500

    Initial Commit

---
+++ Aprendiendo-SwiftIU/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Aprendiendo-SwiftIU
+//
+//  Created by Luis Carlos Mejia Garcia on 23/01/20.
+//  Copyright © 2020 Mejia Garcia. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
